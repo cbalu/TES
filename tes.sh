0bf00016591af1db744aebdd185d0d983d0b1f80
Balasubramanian C <cbalucommit@github>
2014-06-23 11:06:11 +0530

    Added license information

--- tes.sh
+++ tes.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 #
 # TES - Trip Expense Splitter
+# Copyright (C) 2014  BalaC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Date Started: 05/06/2014 (DD/MM/YYYY)
 #
@@ -459,6 +473,12 @@ function check_prerequisite {
 	if [[ $? -eq 0 ]]; then
 		var_tput_found=1
 	fi
+	# Show license first then proceed
+	echo "TES  Copyright (C) 2014  BalaC"
+	echo "This program comes with ABSOLUTELY NO WARRANTY;"
+	echo "This is free software, and you are welcome to redistribute it"
+	echo "under certain conditions;"
+	echo ""
 	echo "=========================================================="
 	echo "#### INITIATING PREREQUISITE CHECK TO RUN APPLICATION ####"
 	echo "=========================================================="
